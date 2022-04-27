4206a66d3e59d891ce96149c52264ee1bcb4ead9
Valeriia Dubrovskaia <sphen0@yandex.ru>
2022-04-27 21:02:30 +0300

    authorization

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Layout } from '../Layout/Layout';
 import { Login } from '../Login/Login';
 import { NotFound } from '../NotFound/NotFound';
@@ -8,7 +8,32 @@ import { NotFound } from '../NotFound/NotFound';
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') ? JSON.parse(localStorage.getItem('isLoggedIn')) : false)
+  const [name, setName] = useState(localStorage.getItem('name') ? JSON.parse(localStorage.getItem('name')) : '')
+  const [password, setPassword] = useState(localStorage.getItem('password') ? JSON.parse(localStorage.getItem('password')) : '')
+
+  const handleLoggedIn = () => {
+    setIsLoggedIn(true);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn));
+  }, [isLoggedIn]);
+
+
+  const handleLogIn = ({ login, password }) => {
+
+    handleLoggedIn()
+    setName({ login })
+    setPassword({ password })
+
+  }
+
+  useEffect(() => {
+    localStorage.setItem("login", JSON.stringify(name));
+    localStorage.setItem("password", JSON.stringify(password));
+  }, [name, password]);
+
 
   return (
     <>
@@ -17,7 +42,7 @@ function App() {
 
         <Route path="/" element={<Layout isLoggedIn={isLoggedIn} />}>
 
-          <Route path="signin" element={<Login isLoggedIn={isLoggedIn} /*onLogIn={handleLogIn} err={errMessage}*/ />} />
+          <Route path="signin" element={<Login onLogIn={handleLogIn} />} />
 
         </Route>
 
