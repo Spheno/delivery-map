cae3959af3036648b69e8d53f2d2c77a8018b401
Valeriia Dubrovskaia <sphen0@yandex.ru>
2022-04-27 16:44:33 +0300

    login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,40 +2,53 @@ import './Login.css';
 
 export function Login({ /*onLogIn, err,*/ isLoggedIn }) {
 
- /* const navigate = useNavigate();
-
-  useEffect(() => {
-    if(isLoggedIn) {
-      navigate("/movies")
-    }
-  }, [])
-
-  const { values, handleChange, errors, isValid } = useFormWithValidation();
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    onLogIn(
-      {
-        email: values.email,
-        password: values.password
-      }
-    );
-  }*/
+  /* const navigate = useNavigate();
+ 
+   useEffect(() => {
+     if(isLoggedIn) {
+       navigate("/movies")
+     }
+   }, [])
+ 
+   const { values, handleChange, errors, isValid } = useFormWithValidation();
+ 
+   function handleSubmit(e) {
+     e.preventDefault();
+     onLogIn(
+       {
+         email: values.email,
+         password: values.password
+       }
+     );
+   }*/
 
   return (
 
     <main className="page__container">
-    <section className="form">
-      <h2 className="form__title">Авторизация</h2>
-      <form name="signin" className="form__container" /*onSubmit={(e) => handleSubmit(e)}*/ >
-        <input type="text" name="login" className="form__input" id="login"
-          placeholder="login" minLength="2" maxLength="30" required /*onChange={(e) => handleChangeEmail(e)}*/ />
-        <input type="password" name="password" className="form__input" id="password"
-          placeholder="Пароль" required /*onChange={(e) => handleChangePassword(e)}*/ />
-        <button className="form__button-submit form__button-submit_type_signin" type="submit">Войти</button>
-      </form>
-    </section>
-  </main>
+      <section className="form">
+        <h2 className="form__title">Авторизация</h2>
+        <form name="signin" className="form__container" /*onSubmit={(e) => handleSubmit(e)}*/ >
+          <label className="form__label" htmlFor="login">
+            Login
+            <input type="text" name="login" className="form__input" id="login"
+              placeholder="Login" minLength="2" maxLength="30" required /*onChange={(e) => handleChangeEmail(e)}*/ />
+            <span className="form__input-error" id="login-error">
+              {/*errors.email || ""*/}
+            </span>
+          </label>
+          <label className="form__label" htmlFor="password">
+            Пароль
+            <input type="password" name="password" className="form__input" id="password"
+              placeholder="Пароль" required /*onChange={(e) => handleChangePassword(e)}*/ />
+            <span className="form__input-error" id="password-error">
+              {/*errors.password || ""*/}
+            </span>
+          </label>
+
+          <button className="form__button-submit form__button-submit_type_signin hover-button" type="submit">Войти</button>
+        </form>
+      </section>
+    </main>
 
   );
 
