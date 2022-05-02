678889904ceac5c5f6a267676ecf97682890a2cb
Valeriia Dubrovskaia <sphen0@yandex.ru>
2022-05-02 19:00:23 +0300

    fix login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -28,21 +28,24 @@ export function Login({ onLogIn, isLoggedIn }) {
 
   return (
 
-    <main className="page__container">
-      <section className="form">
+    <main className="page__container login">
+
+      <section className="form form_type_login">
         <h2 className="form__title">Авторизация</h2>
         <form name="signin" className="form__container" onSubmit={(e) => handleSubmit(e)} >
-          <label className="form__label" htmlFor="login">
+
+          <label className="form__field" htmlFor="login">
             Login
-            <input type="text" name="login" className="form__input" id="login"
+            <input type="text" name="login" className="form__input form__input_login" id="login"
               placeholder="Login" minLength="2" maxLength="30" required value={values.login || ""} onChange={(e) => handleChange(e)} />
             <span className="form__input-error" id="login-error">
               {errors.login || ""}
             </span>
           </label>
-          <label className="form__label" htmlFor="password">
+
+          <label className="form__field" htmlFor="password">
             Пароль
-            <input type="password" name="password" className="form__input" id="password"
+            <input type="password" name="password" className="form__input form__input_password" id="password"
               placeholder="Пароль" required value={values.password || ""} onChange={(e) => handleChange(e)} />
             <span className="form__input-error" id="password-error">
               {errors.password || ""}
@@ -52,6 +55,7 @@ export function Login({ onLogIn, isLoggedIn }) {
           <button className={`form__button-submit form__button-submit_type_signin ${isValid ? 'hover-button' : 'button__disabled'}`} type="submit">Войти</button>
         </form>
       </section>
+
     </main>
 
   );
